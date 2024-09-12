8f4ff0be5750b2ee7069cdbd43d2a697be86534d
Suraj Khanal Chetri <kc.suraz@gmail.com>
2024-09-12 12:52:09 +0200

    Update ReloadSwitch.ts

--- src/devices/ReloadSwitch.ts
+++ src/devices/ReloadSwitch.ts
@@ -1,6 +1,6 @@
 import OneFunctionSwitch from './OneFunctionSwitch';
 import KAKUPlatform from '../KAKUPlatform';
-import {PlatformAccessory} from 'homebridge';
+import { PlatformAccessory } from 'homebridge';
 
 export default class ReloadSwitch extends OneFunctionSwitch {
   constructor(
@@ -17,8 +17,9 @@ export default class ReloadSwitch extends OneFunctionSwitch {
     await super.onSet();
     try {
       await this.platform.setup();
+      this.platform.logger.info('Platform setup successfully after reload switch was toggled.');
     } catch (e) {
-      this.platform.logger.error(`Error running setup after reload switch toggled: ${e}`);
+      this.platform.logger.error(`Error running setup after reload switch toggled: ${e.message}`);
     }
   }
 }
